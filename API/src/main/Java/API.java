306144277a0a4b1b12a753b0bbf0dc9e0a6c6e9d
Solvegam <solvegam2011@yandex.ua>
2015-06-03 00:06:15 +0300

    FirstHomeTask was added

---
+++ API/src/main/Java/API.java
@@ -0,0 +1,11 @@
+package Java;
+
+/**
+ * Created by Stas on 02.06.2015.
+ */
+interface API
+{
+    void addSomethingCool();
+    void getSomethingNotSoCool();
+    void deleteWhatEverYouWant();
+}
---
+++ WEBservice/src/main/Java/APIimplementation.java
@@ -0,0 +1,11 @@
+package java;
+
+/**
+ * Created by Stas on 02.06.2015.
+ */
+public class APIimplementation implements API {
+
+    void addSomethingCool(){};
+    void getSomethingNotSoCool(){};
+    void deleteWhatEverYouWant(){};
+}
