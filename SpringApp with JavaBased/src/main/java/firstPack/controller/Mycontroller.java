45b3a7321136d81c8a22bbb2109812f6b6d1b67e
Solvegam <solvegam2011@yandex.ua>
2015-08-05 06:34:28 +0300

    Just trying to make it work

---
+++ SpringApp with JavaBased/src/main/java/firstPack/controller/Mycontroller.java
@@ -0,0 +1,22 @@
+package firstPack.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Stas on 02.08.2015.
+ */
+
+@Controller
+@RequestMapping("/start/")
+
+public class Mycontroller {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        model.addAttribute("message", "If you want to start – just wait");
+        return "start";
+    }
+}
